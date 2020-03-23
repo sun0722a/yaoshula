c90e6b44e5010a1ab74a1186850f1bcad1210635
annie <sun0722a@gmail.com>
2020-03-23 19:20:31 +0800

    沒事

--- src/main/webapp/js/_05_product/productList.js
+++ src/main/webapp/js/_05_product/productList.js
@@ -2,12 +2,15 @@ function doFirst() {
   nowPage = document.getElementById("nowPage");
   pageForm = document.getElementById("pageForm");
   searchForm = document.getElementById("searchForm");
+  // nowPage = document.getElementById("nowPage");
 
   searchForm.addEventListener("change", function() {
     searchForm.submit();
   });
-  nowPage.addEventListener("change", function() {
-    pageForm.submit();
-  });
+  nowPage.addEventListener("select", changea);
+}
+function changea() {
+  alert("change");
+  // pageForm.submit();
 }
 window.addEventListener("load", doFirst);
